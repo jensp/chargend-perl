78317feb89620ba115f52cefc0ecf0d2272a3ce6
jensp <jens@jenux.homelinux.org>
2009-08-28 17:27:30 +0200

    initial import

---
+++ chargend.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+# Copyleft 2009 Jens Pranaitis <jens@chaox.net>
+# largly based on the example by Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
+# This program may be used, modified, and distributed under the same
+# terms as Perl itself. Please see the license that came with your Perl
+# distribution for details.
+use POE qw(Component::Server::Chargen);
+if ($#ARGV != 2 ) {
+  print "Usage: $0 address port\n";
+  exit;
+}
+$bind_addr = $ARGV[0];
+$bind_port = $ARGV[1];
+my ($object) = POE::Component::Server::Chargen->spawn( Alias => 'Echo-Server', 
+                                                       BindAddress => $bind_addr, 
+                                                       BindPort => $bind_port, 
+                                                       options => { trace => 1 } );
+POE::Session->create(
+	inline_states => { _start => \&simple_start,
+			   _stop  => \&simple_stop, },
+);
+
+$poe_kernel->run();
+exit 0;
+
+sub simple_start {
+}
+
+sub simple_stop {
+}
